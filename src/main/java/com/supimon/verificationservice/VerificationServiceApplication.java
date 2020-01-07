f8f2c98b2512ba6a997241137842f5b47dc253f3
Supi <supimon@gmail.com>
2020-01-07 13:28:23 +0530

    made the service discoverable via Eureka client. POC finished.

--- src/main/java/com/supimon/verificationservice/VerificationServiceApplication.java
+++ src/main/java/com/supimon/verificationservice/VerificationServiceApplication.java
@@ -5,11 +5,13 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VerificationServiceApplication {
 
 	public static void main(String[] args) throws IOException {
